1e613a5bafbbb1ba2af2f25b36183470d6f22554
Abdulaziz <abdulaziz221201@gmail.com>
2021-02-06 21:42:21 +0500

    ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductPage';
+
 
 function App() {
   return (
-   <>
+   <Router>
+   <Header />
+   <main className='py-3'>
 
-   </>
+    <Container>
+
+    <Route path='/' component={HomePage} exact />
+    <Route path='/product/:id' component={ProductsPage} />
+
+    </Container>
+
+   </main>
+
+   <Footer />
+
+   </Router>
   );
 }
 
